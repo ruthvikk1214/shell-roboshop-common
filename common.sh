a4bb6d4e1b0c37deb07bcca2e2935d8a71ee7bce
ruthvekh <ruthvikk1214@gmail.com>
2026-02-24 03:13:45 +0530

    variables

--- common.sh
+++ common.sh
@@ -36,3 +36,34 @@ TOTAL_TIME=$(( $END_TIME - $START_TIME))
 echo "$(date "+%Y-%m-%d $H:%M:%S") | The script got executed in $TOTAL_TIME seconds" | tee -a $LOGS_FILE
 } 
 
+
+User_creation(){
+id roboshop
+if [ $? -ne 0 ]; then
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Creating app user"
+else 
+echo "The roboshop user is already created.. "
+fi
+    
+}
+
+App_installation(){
+
+    mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? " Downloading the code"
+
+cd /app 
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing content"
+
+unzip /tmp/catalogue.zip
+VALIDATE $? "Unzipping the app files in app directory"
+
+}
